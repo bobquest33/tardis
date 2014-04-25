2404573cd2c1204fd6ff491b1f8185edfcf01ed4
Tom Burns <tom.burns@jadedpixel.com>
2014-04-25 02:26:10 +0000

    upport both timestamp formats & test

--- global.go
+++ global.go
@@ -8,7 +8,10 @@ func Epoch(event_time string) (int64, error) {
 	// "2014-04-03T20:39:54+00:00"
 	parsed, err := time.Parse("2006-01-02T15:04:05+00:00", event_time)
 	if err != nil {
-		return 0, err
+		parsed, err = time.Parse("2006-01-02T15:04:05Z", event_time)
+		if err != nil {
+			return 0, err
+		}
 	}
 	return parsed.Unix(), nil
 }
