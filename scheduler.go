19c6d232d3c8148a2b1edf4480c86a7be8f17f34
Tom Burns <tom.burns@jadedpixel.com>
2014-05-13 02:22:26 +0000

    scheduler grabs redis connections as needed to be threadsafe

--- scheduler.go
+++ scheduler.go
@@ -1,44 +1,60 @@
 package tardis
 
 import (
-	"time"
+  "fmt"
+  "time"
 )
 
 type Scheduler struct {
-	Set
-	Period  time.Duration
-	Execute func(job string, timestamp int64) error
+  Key     string
+  Conn    *RedisConn
+  Period  time.Duration
+  Execute func(job string, timestamp int64) error
 }
 
 func (s *Scheduler) Start() chan bool {
-	shutdown := make(chan bool, 1)
-	go s.run(shutdown)
-	return shutdown
+  shutdown := make(chan bool, 1)
+  go s.run(shutdown)
+  return shutdown
 }
 
 func (s *Scheduler) run(shutdown chan bool) {
-	conn := s.Conn
-	defer conn.Close()
-	timeout := make(chan bool, 1)
+  timeout := make(chan bool, 1)
 
-	for true {
-		go func() {
-			time.Sleep(s.Period)
-			timeout <- true
-		}()
+  for true {
+    go func() {
+      time.Sleep(s.Period)
+      timeout <- true
+    }()
 
-		select {
-		case <-shutdown:
-			return
-		case <-timeout:
-			go s.runJobs()
-		}
-	}
+    select {
+    case <-shutdown:
+      return
+    case <-timeout:
+      go s.runJobs()
+    }
+  }
 }
 
 func (s *Scheduler) runJobs() error {
-	err := s.Expire(time.Now().Unix(), func(set string, key string, value int64) error {
-		return s.Execute(key, value)
-	})
-	return err
+  conn, err := s.Conn.Conn()
+  if err != nil {
+    return err
+  }
+  set := &Set{Key: s.Key, Conn: conn, TrackingKey: fmt.Sprintf("scheduler-%s-tracking", s.Key)}
+  defer conn.Close()
+  err = set.Expire(time.Now().Unix(), func(set string, key string, value int64) error {
+    return s.Execute(key, value)
+  })
+  return err
+}
+
+func (s *Scheduler) Add(value string, score int64) error {
+  conn, err := s.Conn.Conn()
+  if err != nil {
+    return err
+  }
+  set := &Set{Key: s.Key, Conn: conn, TrackingKey: fmt.Sprintf("scheduler-%s-tracking", s.Key)}
+  defer conn.Close()
+  return set.Add(value, score)
 }
